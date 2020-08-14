54a1e01e3db58267c122d4ce3e6226c47ec13012
Iban Moreno Martin <iban.moreno.martin@gmail.com>
2020-08-14 10:39:43 +0200

    housekeeping :)

--- proyectosdevilbox/formacion-peripatetica-api/src/controllers/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './ping.controller';
-export * from './course.controller';
-export * from './courses-lessons.controller';
-export * from './lessons-courses.controller';
-export * from './lessons.controller';
-export * from './user.controller';
--- proyectosdevilbox/formacion-peripatetica-api/src/models/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './courses.model';
-export * from './lessons.model';
