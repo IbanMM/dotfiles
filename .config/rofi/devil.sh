1547b9003bec0fc54314e0edc4b4a2f970b2bdd1
Iban Moreno <iban.moreno.martin@gmail.com>
2020-08-23 12:29:47 +0200

    video endoding improved && main machine config

--- .config/rofi/devil.sh
+++ .config/rofi/devil.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-options="$(echo ~/proyectosdevilbox/*)"
+options="$(echo /mnt/amparostorage/proyectosdevilbox*)"
 themes_dir=$HOME/.config/rofi
 theme=${1:-$themes_dir/slate_power.rasi}
-selection=$(find ~/proyectosdevilbox -maxdepth 1 -type d -not -path '*/\.*' | rofi -dmenu -config $theme -columns 1 -lines 15 -width 35)
+selection=$(find /mnt/amparostorage/proyectosdevilbox -maxdepth 1 -type d -not -path '*/\.*' | rofi -dmenu -config $theme -columns 1 -lines 15 -width 35)
 d="htdocs"
 
 echo "This is your selection: $selection"
